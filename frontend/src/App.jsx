b869729ae8aa94e1910ffd13f9daaaca7e5a6994
sillyveira <wslc@cin.ufpe.br>
2025-06-18 18:00:33 -0300

    init: Commit inicial

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className=''>Página inicial</div>
+    </>
+  )
+}
+
+export default App
