4c301f005fe0db91d75f17bc2254c7e64e864620
sillyveira <wslc@cin.ufpe.br>
2025-06-18 18:27:26 -0300

    feat: Footer (em construção)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import Header from './componentes/Header'
+import Footer from './componentes/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Header/>
-    <div className=''>Página inicial</div>
+      <div className='h-max'>
+        <Header />
+        <div className=''>Página inicial</div>
+        <Footer />
+      </div>
     </>
   )
 }
---
+++ frontend/src/componentes/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer className="mt-16 py-8 border-t border-gray-200">
+            <div className="text-center text-gray-600">
+                <p>Footer</p>
+            </div>
+        </footer>
+    )
+}
