729e09db9df3c84cd97e0c3a192d758b3a993bc5
Wesley Silveira <wslc@cin.ufpe.br>
2025-07-05 00:45:04 -0300

    feat: roteamento das páginas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './componentes/Header'
 import Footer from './componentes/Footer'
+import Home from './pages/Home'
+import Descobrir from './pages/Descobrir'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    
-      <div className='h-max'>
+    <Router>
+      <div className='h-screen flex flex-col'>
         <Header />
-        <div className=''>Página inicial</div>
-        <Footer />
+        <div className='flex-1 overflow-hidden'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/descobrir" element={<Descobrir/>}></Route>
+          </Routes>
+        </div>
       </div>
+      
+    </Router>
   )
 }
 
