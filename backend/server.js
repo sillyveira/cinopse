4cbb1f91148eff8772e1c45f402576a597420a07
Wesley Silveira <wslc@cin.ufpe.br>
2025-07-05 22:33:58 -0300

    chore: adicionando as rotas ao app.use()

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,16 @@ const { connectToDatabase } = require('./models/database');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
 
+
+const categoriaRoutes = require('./routes/categoriaRoutes');
+const livroRoutes = require('./routes/livroRoutes');
+
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors({
   origin: 'http://localhost:5173',
-  credentials: true, // Permite enviar cookies
+  credentials: true, // para conseguir enviar cookies
 }));
 
 app.use(cookieParser());
@@ -26,6 +30,9 @@ connectToDatabase();
 
 // Rotas
 app.use('/auth', authRoutes);
+app.use('/categorias', categoriaRoutes);
+app.use('/livros', livroRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Servidor ouvindo na porta ${PORT}`);
