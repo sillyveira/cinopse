d47833e439e94bcd99d945906e26b340410b04b9
Thiago_Fernandes_Lopes_S <165108906+thiagofperaltinha@users.noreply.github.com>
2025-07-15 11:08:53 -0300

    feat: schema de vendas

---
+++ backend/models/venda.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const venda_schema = new mongoose.Schema({
+    compradorID: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    VendedorID: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}, 
+    LivroId: { type: mongoose.Schema.Types.ObjectId, ref: 'Livro', required: true},
+    dataexpira: { type: Date },
+    datapagamento: { type: Date}, 
+    datareserva: {
+        type: Date,
+        default: Date.now
+    },
+    status: {
+        type: String,
+        enum: ['reservado', 'confirmado', 'expirado', 'cancelado'],
+        default: 'reservado'
+    },
+    Avaliacao: {
+        nota: Number,
+        comentario: string 
+    }
+}) 
+
+module.exports = mongoose.model('Venda', venda_schema)
