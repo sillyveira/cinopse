6af085ed853b5c4cda81232861ad3ad3fcd06557
Thiago_Fernandes_Lopes_S <165108906+thiagofperaltinha@users.noreply.github.com>
2025-07-16 18:35:39 -0300

    docs: explicação sobre como vou implementar o prazo de reserva

--- backend/controllers/reservaController.js
+++ backend/controllers/reservaController.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 async function validar_reserva(LivroId){
     const livro_doc = await Livro.findById(LivroId)
     if(!livro_doc)throw new Error('Livro não encontrado')  //Verificando se o livro existe
+    if(!livro_doc.vendedor) throw new Error('O livro não possui vendedor válido')
     if(!livro_doc.disponibilidade) throw new Error('Livro indisponível para reserva')  // Verificando se o Livro ja está reservado
     return livro_doc
 }
@@ -13,8 +14,8 @@ async function criar_nova_reserva({reservadorId, vendedorId, LivroId}){
     return new Reserva({
         reservadorid: reservadorId,
         vendedorid: vendedorId,
-        data_exp: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000), // [PROVISÓRIO] a reserva será desfeita após um tempo, vou implementar mais adiante
-        status_r: true,
+        data_exp: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000), // [PROVISÓRIO] a reserva será desfeita após um tempo, vou implementar mais adiante                                                 
+        status_r: true,                                           // vou utilizar uma biblioteca chamada nodecron pra deletar a reserva após o tempo expirar
         livroid: LivroId,
     })
 }
@@ -27,6 +28,7 @@ exports.criarReserva = async (req, res) => {
         const livro = await validar_reserva(LivroId)
         
         const vendedorId = livro.vendedor
+        
         const reservadorId = req.user.id
         const nova_reserva = await criar_nova_reserva({reservadorId, vendedorId, LivroId})
 
@@ -40,7 +42,7 @@ exports.criarReserva = async (req, res) => {
            
             if(erro.message === 'Livro não encontrado'){ return res.status(404).json({ erro: erro.message })}
             if(erro.message === 'Livro indisponível para reserva'){return res.status(400).json({ erro: erro.message })}
-           
+            if(erro.nessage === 'O livro não possui vendedor válido'){ return res.status(404).json({ erro: erro.message })}
             return res.status(500).json({ erro: '[ERRO]: ao reservar Livro'})
         }
 }
