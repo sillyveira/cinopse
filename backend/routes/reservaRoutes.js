27da3dc0f0ee9da2e468003a09cba0246cf935ff
Thiago_Fernandes_Lopes_S <165108906+thiagofperaltinha@users.noreply.github.com>
2025-07-28 20:47:12 -0300

    fix: corrigindo rotas de reserva

--- backend/routes/reservaRoutes.js
+++ backend/routes/reservaRoutes.js
@@ -4,7 +4,8 @@ const auth = require('../middlewares/auth')
 
 const router = express.Router()
 // Criar Reserva
-router.post('/reserva/:LivroId', auth, reservaController.criarReserva)
+router.post('/:LivroId', auth, reservaController.criarReserva)
+
 // Cancelar Reserva
-router.post('cancelarReserva/:reservaId/:livroId', auth, reservaController.cancelar_reserva)
+router.post('cancelarReserva/:reservaId', auth, reservaController.cancelar_reserva)
 module.exports = router
